f84d9c8dfe9e795b15c20d45f458038ddd94c45c
Alexander Stein <anstein@aphrodite.med.cornell.edu>
2022-03-15 11:30:12 -0400

    Created interactive plot creation R script

---
+++ inst/RScript/interactivePlot.R
@@ -0,0 +1,34 @@
+library('plotly')
+library('htmlwidgets')
+library('RCurl')
+
+args<-commandArgs(trailingOnly=TRUE);
+image_file <- args[1]
+txt <- RCurl::base64Encode(readBin(image_file, "raw", file.info(image_file)[1, "size"]), "txt")
+p <- plot_ly(x = 0:4, y = 0:4,
+             type = 'scatter', mode = 'markers', alpha = 0) %>%
+    layout(
+        images = list(
+            list(
+                source =  paste('data:image/jpg;base64', txt, sep=','),
+                xref = "x",
+                yref = "y",
+                x = 0,
+                y = 4,
+                sizex = 2,
+                sizey = 4,
+                sizing = "stretch",
+                opacity = 1,
+                layer = "below"
+            )
+        )
+    )
+#m = list(r=0, l=0, b=0, t=0)
+fig <- p %>% #layout(margin = m) %>%
+    layout(plot_bgcolor='#ffff',
+           xaxis = list(showgrid = FALSE,showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE,showticklabels = FALSE)
+    )
+fig
+f<-args[2]
+htmlwidgets::saveWidget(fig, file.path(normalizePath(dirname(f)),basename(f)))
